6efae2f03497d5e84d4e797475684581466a9ea1
luke <lukes611@gmail.com>
2017-02-27 00:02:44 +1000

    starting to write app

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ angular.module('LDiff', []);
 angular.module('LDiff').controller('LDiff',
 ['$scope', function($scope){
     $scope.appName = 'q-diff';
-    
+    $scope.input1 = 'hi\nthere\nyou are cool';
+    $scope.input2 = 'hi\nthere2\nyou are cool';
     
 }]);
