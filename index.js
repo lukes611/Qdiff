99613f3a843eda8c731abbb96e00b24c4b261174
luke <lukes611@gmail.com>
2017-03-04 00:14:40 +1000

    cleaner

--- index.js
+++ index.js
@@ -26,18 +26,12 @@ angular.module('LDiff').controller('LDiff',
         var N = Math.max(lines1.length, lines2.length);
         var output = [], i = 0;
         
-        var l1 = 0, l2 = 0, lowestScore, index, j;
+        var l1 = 0, l2 = 0, index, j;
         var states = gen2DArray(N, N, Number.MAX_VALUE);
-        
-        /*for each y state
-            for each x state
-                get smallest dist + my cost -> set output. record output
-        */
-        var minDist = 0, minIndex = 0, tmp;
         var right, down, diag;
         
-        for(l1 = states.length-1; l1 >= 0; l1--){
-            for(l2 = states[l1].length-1; l2 >= 0; l2--){
+        for(l1 = N-1; l1 >= 0; l1--){
+            for(l2 = N-1; l2 >= 0; l2--){
                 
                 var h1 = l1 < lines1.length;
                 var h2 = l2 < lines2.length;
@@ -51,7 +45,6 @@ angular.module('LDiff').controller('LDiff',
                     states[l1][l2] = {cost : cc, next : undefined, op : tmp};
                     continue;
                 }
-                
                 //get old cost and next
                 right = (l2 + 1 >= states[l1].length) ? {cost:Number.MAX_VALUE, next:undefined, op:undefined}
                 : {cost:states[l1][l2+1].cost + cc, next:states[l1][l2+1], op:'add'};
@@ -69,16 +62,13 @@ angular.module('LDiff').controller('LDiff',
             }
         }
         
-        
-        
-        
-        for(i = 0; i < states.length; i++){
+        /*for(i = 0; i < states.length; i++){
             var out = i + '.: ';
             for(var j = 0; j < states[i].length; j++){
                 out += states[i][j].cost + ' . ';
             }
             console.log(out);
-        }
+        }*/
         
         var start = states[0][0];
         while(start){
@@ -94,14 +84,14 @@ angular.module('LDiff').controller('LDiff',
             var o = output[i];
             //console.log(o)
             if(o.op == 'del' && l1 < lines1.length){
-                console.log('del', lines1[l1]);
+                //console.log('del', lines1[l1]);
                 if(ret[ret.length-1].type == 'del')
                     ret[ret.length-1].str.push(lines1[l1]);   
                 else ret.push({type:'del', str:[lines1[l1]]});
                 l1++;  
             } 
             else if(o.op == 'add' && l2 < lines2.length){ 
-                console.log('add', lines2[l2]);
+                //console.log('add', lines2[l2]);
                 if(ret[ret.length-1].type == 'add')
                     ret[ret.length-1].str.push(lines2[l2]);   
                 else ret.push({type:'add', str:[lines2[l2]]});
@@ -113,15 +103,13 @@ angular.module('LDiff').controller('LDiff',
                     if(ret[ret.length-1].type == 'keep')
                         ret[ret.length-1].str.push(l);   
                     else ret.push({type:'keep', str:[l]});
-                    console.log('keep', l);
+                    //console.log('keep', l);
                 }
                 l1++;l2++;
             } 
         }
         ret.shift();
         $scope.output = ret;
-        ret.forEach(x => console.log(x));
-        //solveStates(states);
     };
     
     $scope.getLineCSS = function(type){
@@ -131,7 +119,7 @@ angular.module('LDiff').controller('LDiff',
         return {'background-color' : color, border: '1px solid pink'};
     };
 
-    
+    //$scope.process();
     
     
 }]);
